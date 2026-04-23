8ecb4ddba76b674eca03304e2112ded70461cfab
bartekstpn <bartekstpn@gmail.com>
2026-04-23 16:43:50 +0200

    Dodano sidebar

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,51 @@
 import "./App.css";
 import { useState, useEffect, useRef } from "react";
 import { generateClient } from "aws-amplify/api";
+import { Authenticator, ThemeProvider } from "@aws-amplify/ui-react";
+import "@aws-amplify/ui-react/styles.css";
 
-function App() {
+const theme = {
+  name: "dark-theme",
+  tokens: {
+    colors: {
+      background: {
+        primary: { value: "#0f172a" },
+        secondary: { value: "#ffffff" },
+      },
+      font: {
+        primary: { value: "#ffffff" },
+      },
+      brand: {
+        primary: {
+          80: { value: "#ffffff" },
+          secondary: { value: "#94a3b8" }
+        },
+      },
+    },
+    components: {
+      tabs:
+      {
+        item: {
+          color: { value: "#ababab" },
+          _active: {
+            color: { value: "#ffffff" }
+          },
+        },
+      },
+      button: {
+        primary: {
+          backgroundColor: { value: "#0f172a" },
+          _hover: {
+            backgroundColor: { value: "#ffffff" },
+          },
+        },
+      },
+    }
+  },
+};
+
+function App({ signOut, user }) {
   const client = generateClient();
-
   const [notes, setNotes] = useState([]);
   const [input, setInput] = useState("");
   const [editingId, setEditingId] = useState(null);
@@ -79,6 +120,7 @@ function App() {
       await client.models.Note.delete({ id });
       setNotes((prev) => prev.filter((n) => n.id !== id));
       setDeletingId(null);
+      setInput("");
     }, 300);
   }
 
@@ -96,79 +138,107 @@ function App() {
   }
 
   return (
-    <div
-      className="container"
-      onClick={() => {
-        setEditingId(null);
-        setInput("");
-      }}
-    >
-      <h1>📝 Notes App</h1>
+    <div className="layout">
 
-      <div className="inputRow">
-        <input
-          value={input}
-          onChange={(e) => setInput(e.target.value)}
-          onClick={(e) => e.stopPropagation()}
-          placeholder="Wpisz notatkę..."
-        />
+      <div className="sidebar">
+        <div className="avatar">
+          {user?.signInDetails?.loginId?.[0]?.toUpperCase()}
+        </div>
+        <div className="userSection">
+          <div className="email">
+            {user?.signInDetails?.loginId}
+          </div>
 
-        <button
-          onClick={(e) => {
-            e.stopPropagation();
-            editingId ? updateNote() : addNote();
-          }}
-        >
-          {editingId ? "Zapisz" : "Dodaj"}
-        </button>
+          <button className="logoutBtn" onClick={signOut}>
+            Wyloguj
+          </button>
+        </div>
       </div>
 
-      <div className="notesList" ref={listRef}>
-        {notes.map((n) => (
-          <div
-            className={`noteCard 
-              ${editingId === n.id ? "active" : ""} 
-              ${deletingId === n.id ? "removing" : ""}`}
-            key={n.id}
+      <div
+        className="container"
+        onClick={() => {
+          setEditingId(null);
+          setInput("");
+        }}
+      >
+        <h1>📝 Notes App</h1>
+
+        <div className="inputRow">
+          <input
+            value={input}
+            onChange={(e) => setInput(e.target.value)}
+            onClick={(e) => e.stopPropagation()}
+            placeholder="Wpisz notatkę..."
+          />
+
+          <button
             onClick={(e) => {
               e.stopPropagation();
-              setInput(n.name);
-              setEditingId(n.id);
+              editingId ? updateNote() : addNote();
             }}
           >
-            <span
-              onClick={() => {
+            {editingId ? "Zapisz" : "Dodaj"}
+          </button>
+        </div>
+
+        <div className="notesList" ref={listRef}>
+          {notes.map((n) => (
+            <div
+              className={`noteCard 
+              ${editingId === n.id ? "active" : ""} 
+              ${deletingId === n.id ? "removing" : ""}`}
+              key={n.id}
+              onClick={(e) => {
+                e.stopPropagation();
                 setInput(n.name);
                 setEditingId(n.id);
               }}
             >
-              {n.name}
-            </span>
+              <span
+                onClick={() => {
+                  setInput(n.name);
+                  setEditingId(n.id);
+                }}
+              >
+                {n.name}
+              </span>
 
-            <button
-              onClick={(e) => {
-                e.stopPropagation();
-                deleteNote(n.id);
-              }}
-            >
-              ❌
-            </button>
-          </div>
-        ))}
+              <button
+                onClick={(e) => {
+                  e.stopPropagation();
+                  deleteNote(n.id);
+                }}
+              >
+                ❌
+              </button>
+            </div>
+          ))}
+        </div>
       </div>
     </div>
-  );
-
-
-
-
-
-
-
-
-
-
-
+  )
 }
+export default function AppWrapper() {
+  return (
 
-export default App;
+    <ThemeProvider theme={theme}>
+      <Authenticator
+        components={{
+          Header() {
+            return (
+              <div style={{ textAlign: "center", marginBottom: 20 }}>
+                <h2>📝 Notes App</h2>
+                <p>Zaloguj się, aby kontynuować</p>
+              </div>
+            );
+          },
+        }}
+      >
+        {({ signOut, user }) => (
+          <App signOut={signOut} user={user} />
+        )}
+      </Authenticator>
+    </ThemeProvider>
+  );
+}
--- src/main.jsx
+++ src/main.jsx
@@ -1,7 +1,7 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
-import App from './App.jsx'
+import AppWrapper from './App.jsx'
 
 import { Amplify } from "aws-amplify";
 import outputs from "../amplify_outputs.json";
@@ -10,6 +10,6 @@ Amplify.configure(outputs);
 console.log("CONFIG:", outputs);
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <App />
+    <AppWrapper />
   </StrictMode>,
 )
